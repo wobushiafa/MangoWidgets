d01418e777de3238578239d675c128395d9fc8cd
Alpha <2735903433@qq.com>
2023-02-12 18:03:09 +0800

    添加ProgressRing控件及样式

---
+++ src/MangoWidgets/Converters/BooleanReverseConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MangoWidgets.Converters
+{
+    public class BooleanReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Boolean.TryParse(value?.ToString(),out bool result);
+            return !result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Boolean.TryParse(value?.ToString(), out bool result);
+            return !result;
+        }
+    }
+}
