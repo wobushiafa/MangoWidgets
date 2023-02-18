910aa60a58d7f87b4e8baa9870a7f2dae6f2e01d
Alpha <2735903433@qq.com>
2023-02-18 17:38:03 +0800

    在ViewModel中更优雅地关闭DialogControl

--- src/MangoWidgets.Sample/ViewModels/TestDialogViewModel.cs
+++ src/MangoWidgets.Sample/ViewModels/TestDialogViewModel.cs
@@ -1,8 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MangoWidgets.Sample.ViewModels;
 
 public partial class TestDialogViewModel:ObservableObject
 {
+    [ObservableProperty]
+    private bool? _dialogResult;
 
+
+    [RelayCommand]
+    private void Close()
+    {
+        DialogResult = false;
+    }
 }
--- src/MangoWidgets/Controls/Dialog/DialogControl.cs
+++ src/MangoWidgets/Controls/Dialog/DialogControl.cs
@@ -1,10 +1,27 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using MangoWidgets.Common;
 
 namespace MangoWidgets.Controls;
 
 public class DialogControl:UserControl,IDialogControl
 {
+    public bool? DialogResult
+    {
+        get { return (bool?)GetValue(DialogResultProperty); }
+        set { SetValue(DialogResultProperty, value); }
+    }
+    public static readonly DependencyProperty DialogResultProperty =
+        DependencyProperty.Register("DialogResult", typeof(bool?), typeof(DialogControl), new PropertyMetadata(null, OnDialogResultChangedCallback));
+    private static void OnDialogResultChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is DialogControl ctrl)
+        {
+            _ = bool.TryParse(e.NewValue.ToString(), out bool result);
+            ctrl.Close(result);
+        }
+    }
+
     public event RoutedDialogControlCloseEvent Closed;
 
     public void Close()
